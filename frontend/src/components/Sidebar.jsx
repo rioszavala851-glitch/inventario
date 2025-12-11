17e2dedd91aaa1e0462d16f788a2958d94330c20
alfonsozavala825 <rioszavala851@gmail.com>
2025-12-10 20:35:43 -0800

    Fix: Remove corrupted text from Sidebar.jsx import

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-Papimport { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import {
     LayoutDashboard, Package, ChefHat, Salad, Utensils,
     Archive, Clock, LogOut, Sun, Moon, ChevronRight, ChevronLeft, ShieldCheck, X,
